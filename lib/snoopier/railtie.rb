3905a67dc6af92c6247e74689550a863c52cf535
we5 <bjoern@we5.de>
2015-02-25 14:17:55 +0100

    Enable for Rails 3

--- lib/snoopier/railtie.rb
+++ lib/snoopier/railtie.rb
@@ -4,7 +4,6 @@ module Snoopier
       if ( ( basic_auth_enabled  = ENV["BASIC_AUTH_ENABLED"] ) == "true" &&
            ( basic_auth_user     = ENV["BASIC_AUTH_USER"] ).present? &&
            ( basic_auth_password = ENV["BASIC_AUTH_PASSWORD"] ).present? )
-
         app.middleware.use "::Rack::Auth::Basic", "#{Rails.application.class.parent_name} #{Rails.env}" do |u, p|
           [u, p] == [ basic_auth_user, basic_auth_password ]
         end
--- lib/snoopier/version.rb
+++ lib/snoopier/version.rb
@@ -1,3 +1,3 @@
 module Snoopier
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
